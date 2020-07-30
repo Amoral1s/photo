512c5f893d8c5b0a4cc77927bd60f7fc37b13ded
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-07-30 21:03:54 +0300

    1

--- js/common.js
+++ js/common.js
@@ -28,6 +28,73 @@ $(document).ready(function () {
       $('.header__menu').removeClass('header__menu__active');
     });
   }
- 
+
+  //Lazy load JS Native
+
+  const images = document.querySelectorAll('.portfolio__href img');
+  
+
+  if (images) {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    }
+
+    function handleImg(myImg, observer) {
+        myImg.forEach(myImgSingle => {
+            if (myImgSingle.intersectionRatio > 0) {
+                loadImage(myImgSingle.target);
+            }
+        })
+    }
+
+    function loadImage(image) {
+        image.src = image.getAttribute('data');
+    }
+
+    const observer = new IntersectionObserver(handleImg, options);
+
+    images.forEach(img => {
+        observer.observe(img);
+    });
+  }
+
+  const author = document.querySelectorAll('.about__right img');
+
+  if (author) {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    }
+
+    function handleImg(myImg, observer) {
+        myImg.forEach(myImgSingle => {
+            if (myImgSingle.intersectionRatio > 0) {
+                loadImage(myImgSingle.target);
+            }
+        })
+    }
+
+    function loadImage(image) {
+        image.src = image.getAttribute('data');
+    }
+
+    const observer = new IntersectionObserver(handleImg, options);
+
+    author.forEach(img => {
+        observer.observe(img);
+    });
+  }
+  
+
+
+
+
+
+
+
+
 
 });
